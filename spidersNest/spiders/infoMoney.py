71d8bac7460511c03a8903e6b05aa33f83d08ad2
jgfn1 <jgfn@cin.ufpe.br>
2021-03-29 22:37:51 -0300

    reading all the link in the main page

--- spidersNest/spiders/infoMoney.py
+++ spidersNest/spiders/infoMoney.py
@@ -1,3 +1,12 @@
 import scrapy
 
-class InfoMoneySpider(scrapy.Spider)
+class InfoMoneySpider(scrapy.Spider):
+    name = 'infoMoney'
+    start_urls = ["https://www.infomoney.com.br/"]
+
+    def parse(self, response):
+        article_links = response.css('span.hl-title')
+        article_links = article_links.xpath('.//a/@href')
+        for link in article_links.extract():
+            print('\n')
+            yield {"links": link}
